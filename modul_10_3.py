2522cab4b41c1fb0f022d8739c9beddea4d3db9b
alenash1988 <Aleonash1988@mail.ru>
2024-12-08 17:05:06 +0900

    Add files via upload

--- modul_10_3.py
+++ modul_10_3.py
@@ -4,28 +4,31 @@ import time
 
 lock = threading.Lock()
 
+
 class Bank:
     def __init__(self):
         self.balance = 0
+        self.lock = threading.Lock()
 
     def deposit(self):
-        n = random.randint(50, 500)
-        with lock:
-            if self.balance <= 500 and lock.locked():
-                self.balance += n
-                print(f'Пополнение: {n}. Баланс: {self.balance}')
-        time.sleep(0.001)
+        for i in range(100):
+            n = random.randint(50, 500)
+            with self.lock:
+                if self.balance <= 500 and lock.locked():
+                    self.balance += n
+                    print(f'Пополнение: {n}. Баланс: {self.balance}')
+            time.sleep(0.001)
+
     def take(self):
         for i in range(100):
             n1 = random.randint(50, 500)
             print(f'Запрос на {n1}')
-            with lock:
+            with self.lock:
                 if n1 <= self.balance:
                     print(f'Снятие: {n1}. Баланс: {self.balance - n1}"')
                 else:
                     print(f'Запрос отклонён, недостаточно средств')
             time.sleep(0.001)
-
 
 
 bk = Bank()
